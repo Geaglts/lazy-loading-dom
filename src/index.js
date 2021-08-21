5502209f73822e5bea41b4d2a0738f0a80e5b6e5
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-08-20 22:00:02 -0500

    Ya se agregan las imagenes desde js y ya tienen estilos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import './styles/main.scss';
 
+function generateRandomInt(limit) {
+  return Math.floor(Math.random() * limit);
+}
+
+function getImgUrl() {
+  const LIMIT = 123;
+  const BASE_URL = 'https://randomfox.ca/images';
+  return `${BASE_URL}/${generateRandomInt(LIMIT)}.jpg`;
+}
+
+function createImgElement() {
+  const div = document.createElement('div');
+  const img = document.createElement('img');
+  //div
+  div.appendChild(img);
+  div.classList.add('image-container');
+  // img - hijo
+  img.src = getImgUrl();
+  img.classList.add('image');
+  return div;
+}
+
 function main() {
   const app = document.getElementById('app');
-  console.log(app);
+  const button = document.querySelector('#new-image');
+  button.addEventListener('click', () => {
+    const imageElement = createImgElement();
+    app.appendChild(imageElement);
+  });
 }
 
 main();
