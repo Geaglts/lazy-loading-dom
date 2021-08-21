32ac735b0377a00e155d433af75dd28a5c49dab9
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-08-21 12:25:41 -0500

    Si la imagen no ha cargado muestra un fondo gris

--- src/components/Img.js
+++ src/components/Img.js
@@ -3,6 +3,7 @@ import { getImgUrl } from '../utils/getImgUrl';
 export function Img() {
   const img = document.createElement('img');
   img.classList.add('image');
+  img.classList.add('blur');
   img.dataset.src = getImgUrl();
   return img;
 }
